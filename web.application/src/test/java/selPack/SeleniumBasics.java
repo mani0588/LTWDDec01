b71de55a3f5d5c52ae73dfcde065164dc74f63a3
Manikandan P <mani.alagarece@gmail.com>
2020-12-08 08:53:26 +0530

    Selenium Basics -Launch application

--- web.application/src/test/java/selPack/SeleniumBasics.java
+++ web.application/src/test/java/selPack/SeleniumBasics.java
@@ -1,17 +1,22 @@
 package selPack;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SeleniumBasics {
-	
+
 	public static void main(String[] args) {
-		
-		
+
+		WebDriverManager.chromedriver().setup();
+
 		// Opening browser
 		WebDriver driver = new ChromeDriver();
-		
-		
+
 		// Loading App url
-		
-		
+		driver.get("https://www.mycontactform.com/samples.php");
+
 	}
 
 }
