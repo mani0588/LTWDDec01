eb6c2e39b69ef03c00ab5dfc1b191574f09880f1
Manikandan P <mani.alagarece@gmail.com>
2020-12-08 08:27:57 +0530

    Added all maven dependencies

---
+++ web.application/src/test/java/selPack/SeleniumBasics.java
@@ -0,0 +1,17 @@
+package selPack;
+
+public class SeleniumBasics {
+	
+	public static void main(String[] args) {
+		
+		
+		// Opening browser
+		WebDriver driver = new ChromeDriver();
+		
+		
+		// Loading App url
+		
+		
+	}
+
+}
