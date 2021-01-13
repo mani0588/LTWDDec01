ed8a71cbe7597a6e6af29e4094cac26afb1f640c
Manikandan Periyasamy <mani.alagarece@gmail.com>
2021-01-13 08:37:00 +0530

    Interview QA

--- web.application/src/test/java/testNGPack/SoftAssertExample.java
+++ web.application/src/test/java/testNGPack/SoftAssertExample.java
@@ -1,5 +1,7 @@
 package testNGPack;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -35,23 +37,72 @@ public class SoftAssertExample {
 		
 		
 //		How do you handle the group of elements?
+		// findElements and List<WebElement>
+		
 //		How do you handle the dynamic elements?
+		// id='abc123'
+		// Relative xpath methods like starts-with / contains
+		
 //		What is the difference between close and quit methods?
-//		What is the difference between implicit and explicit wait?
+		// driver.close(); - close current window
+		// driver.quit(); - close all associated windows
+		
+//		What is the difference between implicit, explicit and fluent wait?
+		// implicit - globally applicable in findElement() and based visibility of element
+		// explicit - specific to the element with specific condition
+		// fluent - frequency of verification can be configured and you can ignore the exception during this wait
+		
 //		What are all the different selenium exceptions you faced?
+		// NoSuchElementException - element is not present in html
+		// TimeoutException - selenium waits
+		// StaleElementReferenceException - 
+		// WebElement e = driver.findElement(By.id(""));
+		//.....
+		// page might have refreshed
+		// e.click();
+		// NoActiveAlertException - no alert popup right now
+		 
 //		How do you get the title and url of the browser?
+		// getTitle() and getCurrentUrl()
+		
 //		How do you take the screenshot?
+		// TakesScreenshot and getScreenshotAs()
+		// Shutterbug API or AShot API
+		
 //		Tell us some of the TestNG annotations
+		// beforesuite, beforetest, beforeclass, beforemothod, test + dataProvider, aftermethod, afterclass, aftertest, aftersuite
+		
 //		How many test cases you will automate per day?
+		// 1 or 2 test cases [depends on the complexity]
+		
 //		What is the difference between xpath and CSS selector?
+		// //input[@type='checkbox'] - xpath
+		// input[type='checkbox'] - css
+		
 //		How many locators you have in Selenium? which locators do you use and why?
+		// 8
+		// xpath can be supported all situations, so we prefer to use xpath
+		
 //		What is the difference between / and //?
+		// / to locate only child
+		// // to locate child and grant-child
+		
 //		How do you change the attributes of an element?
+		// JavascriptExecutor and js.executeScript('setAttribute', attname, val);
+		
 //		How do you enter value into text box without using send keys?
 //		(alternative question: Send keys is not working then how do you enter values?)
+		// JavascriptExecutor and js.executeScript('setAttribute', value, 'mani');
+		
 //		How do you handle the webtables? (get number of rows/columns/cells)
+		// driver.fes(By.xpath("//table[@id='customers']//tr")).size();
+		// driver.fes(By.xpath("//table[@id='customers']//tr[1]/th")).size();
+		// driver.fes(By.xpath("//table[@id='customers']//tr/td")).size();
+		
 //		How do you run the failed test cases alone in TestNG?
+		// testng-failed.xml file can be used to rerun all failed test cases
+		
 //		I have a bunch of test cases. I want to run only one test case in first priority. How do you run?
-
+		// @Test(priority=-100000)
 	}
 }
